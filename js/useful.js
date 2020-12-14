55a9da6ae62bd92cf14f1551dafe161870a321df
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2020-12-14 19:35:38 +0100

    [fix] make promise

--- js/useful.js
+++ js/useful.js
@@ -1,28 +1,26 @@
-var HttpClient = function() {
-    this.get = function(aUrl, aCallBack, aErrorCallBack) {
-        try{
-            var anHttpRequest = new XMLHttpRequest();
-            anHttpRequest.onreadystatechange = function() { 
-                if (anHttpRequest.readyState == 4){
-                    if(anHttpRequest.status == 200){
-                        aCallBack(anHttpRequest.responseText);
-                    }else if(anHttpRequest.status == 403){
-                        aErrorCallBack(anHttpRequest.statusText);
-                    }else{
-                        aErrorCallBack();
-                    }
-                }
-            };
-            anHttpRequest.onerror = function(e){
-                aErrorCallBack(e);
-            };
-            anHttpRequest.open("GET", aUrl, true);     
-            anHttpRequest.send(null);
-        }catch(error){
-            aErrorCallBack(error);
+function makeRequest (method, url) {
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.onload = function () {
+        if (this.status >= 200 && this.status < 300) {
+          resolve(xhr.response);
+        } else {
+          reject({
+            status: this.status,
+            statusText: xhr.statusText
+          });
         }
-    }
-}
+      };
+      xhr.onerror = function () {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      };
+      xhr.send();
+    });
+  }
 
 function copy(param){
     let copieur = document.createElement('textarea');
