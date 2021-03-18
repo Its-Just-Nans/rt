d1729f33cf46dd004d98e0011e11574387ef500e
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2021-03-18 09:11:45 +0100

    [fix] change scripts

---
+++ js/raws.js
@@ -0,0 +1,5 @@
+//global variable (kinda bad lol)
+githubName = "its-just-nans";
+repoName = "rt";
+path = `https://api.github.com/repos/${githubName}/${repoName}/contents/`;
+startPath = `https://api.github.com/repos/${githubName}/${repoName}/contents/raws/`;
