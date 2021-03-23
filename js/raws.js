a27656489b59ddf544fe1b059cf0d1819e933512
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2021-03-23 17:24:50 +0100

    [fix] add js in file

--- js/raws.js
+++
@@ -1,4 +0,0 @@
-var githubName = "its-just-nans";
-var repoName = "rt";
-var path = `https://api.github.com/repos/${githubName}/${repoName}/contents/`;
-var startPath = `https://api.github.com/repos/${githubName}/${repoName}/contents/raws/`;
