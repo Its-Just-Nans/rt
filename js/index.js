bffc8e4c09acd4cc93714f52b2b846d70bad34f3
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2021-04-16 08:53:51 +0200

    [fix] change start links

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 const NAME_OF_PROJECT = "rt";
-const DEFAULT_FILE = "about.md";
-const DEFAULT_HASH = "#readme-about";
+const DEFAULT_FILE = "search.html";
+const DEFAULT_HASH = "#search";
 const DOMAIN_NAME = "https://its-just-nans.github.io";
